f9c5cb9d92cdebc1aec92c5acd98a52865308569
fabiotorres95 <engfabiotorres@outlook.com>
2023-12-01 23:32:32 -0300

    home link, h2 titles created

--- news/views.py
+++ news/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from news.models import News
 
 
 def index(request):
-    return render(request, 'home.html')
+    news_data = News.objects.all()
+    news_titles = [news.title for news in news_data]
+    data = {
+        "news_titles": news_titles
+    }
+
+    return render(request, 'home.html', data)
