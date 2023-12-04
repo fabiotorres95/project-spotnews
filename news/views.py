6128bbec2000d68cb8bc663b693f730c43a30c5a
fabiotorres95 <engfabiotorres@outlook.com>
2023-12-04 15:22:08 -0300

    template home.html complete

--- news/views.py
+++ news/views.py
@@ -6,12 +6,14 @@ def index(request):
     news_data = News.objects.all()
     news_list = []
     for data in news_data:
+        parts = str(data.created_at).split('-')
+        converted = f"{parts[2]}/{parts[1]}/{parts[0]}"
         news_list.append(
             {
                 "title": data.title,
                 "content": data.content,
                 "author": data.author,
-                "created_at": data.created_at,
+                "created_at": converted,
                 "image": data.image,
                 "categories": data.categories,
             }
