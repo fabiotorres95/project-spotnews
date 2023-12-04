a9036a0a65c0a2cfd2a8ade341fa028d5749bd04
fabiotorres95 <engfabiotorres@outlook.com>
2023-12-04 15:16:22 -0300

    template home.html done, only missing date format

--- news/views.py
+++ news/views.py
@@ -4,9 +4,21 @@ from news.models import News
 
 def index(request):
     news_data = News.objects.all()
-    news_titles = [news.title for news in news_data]
+    news_list = []
+    for data in news_data:
+        news_list.append(
+            {
+                "title": data.title,
+                "content": data.content,
+                "author": data.author,
+                "created_at": data.created_at,
+                "image": data.image,
+                "categories": data.categories,
+            }
+        )
+
     data = {
-        "news_titles": news_titles
+        "news_list": news_list,
     }
 
     return render(request, 'home.html', data)
